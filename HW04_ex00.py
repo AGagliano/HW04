d9b555edd112d2901dd96e4247ec68b85d021f12
AGagliano <andrea.gagliano@ischool.berkeley.edu>
2015-08-07 14:51:37 -0700

    ex00 submission

--- HW04_ex00.py
+++ HW04_ex00.py
@@ -12,10 +12,33 @@
 ################################################################################
 # Imports
 
+import random
 
 # Body
 
+#Chooses a random integer between 1 and 25, inclusive of 1 and 25
+r = random.randint(1, 25) 	
 
+#Allows user 5 attempts to guess the random number.
+for n in range(5):	
+	#Asks user to guess a number
+	g = raw_input('Take a gander at a number somewhere between 1 and 25:\n')
+	#Tries to convert the string g to an integer, yells at user if input was not an integer.
+	try:
+		g = int(g)
+	except ValueError:
+		print ('Just an integer...make it easy on yourself!')
+	#Tests if user guessed the random number, and gives hints suggesting higher or lower guesses if they didn't.
+	else: 
+		if g == r:
+			print 'Yay you nailed it!'
+			break				#breaks from for loop when user succeeds
+		elif g < r:
+			print "Hint: reach for the sky"
+		else:
+			print "Hint: reach for the ground"
+else:
+	print "Sorry, but we only let you test your luck 5 times!"
 
 
 ################################################################################
